5558587f898cc0f702cba099442232407ebaf444
Daniel Leclair <leclad@uw.edu>
2016-07-12 00:55:57 -0700

    Firebase integrated

---
+++ Pods/Target Support Files/Pods-ThePuzzChat/Pods-ThePuzzChat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ThePuzzChat : NSObject
+@end
+@implementation PodsDummy_Pods_ThePuzzChat
+@end
---
+++ Pods/Target Support Files/Pods-ThePuzzChatUITests/Pods-ThePuzzChatUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ThePuzzChatUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ThePuzzChatUITests
+@end
